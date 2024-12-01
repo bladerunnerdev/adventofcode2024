28fa6b63dd6ef0a5796b0cfb37be643be3b8bf77
Bart Taelemans <99647508+bladerunnerdev@users.noreply.github.com>
2024-12-01 09:40:15 +0100

    Add performance counter

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,26 @@ const dryRun = process.argv[3] !== 'false';
 
 console.log(`Starting puzzle ${puzzleNumber} with dry run: ${dryRun}`);
 
+let puzzle: Puzzle01 | Puzzle02 | null = null;
+
 switch (puzzleNumber) {
     case '01':
-        new Puzzle01(dryRun).solve();
+        puzzle = new Puzzle01(dryRun);
         break;
     case '02':
-        new Puzzle02(dryRun).solve();
+        puzzle = new Puzzle02(dryRun);
         break;
     default:
         console.log('No puzzle number specified, not running anything');
         break;
 }
+
+const startTime = performance.now();
+
+if (puzzle) {
+    puzzle.solve();
+}
+
+const endTime = performance.now();
+
+console.log(`Execution time: ${endTime - startTime}ms`);
